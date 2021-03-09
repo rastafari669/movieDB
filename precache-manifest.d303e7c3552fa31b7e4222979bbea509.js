0e249263e619665bd8127e95c56f5f587b111f9f
hugendubell <joeykraus789@gmail.com>
2021-03-09 16:25:56 +0200

    Updates

---
+++ precache-manifest.d303e7c3552fa31b7e4222979bbea509.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849f6836d9c6545e9216428d2ea17164",
+    "url": "/movieDB/index.html"
+  },
+  {
+    "revision": "1f40fa99d7a060f2c9db",
+    "url": "/movieDB/static/js/2.59876f76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieDB/static/js/2.59876f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda69697f09394092e5f",
+    "url": "/movieDB/static/js/main.7be7240c.chunk.js"
+  },
+  {
+    "revision": "40cb56b3dce126e177c3",
+    "url": "/movieDB/static/js/runtime-main.f48607b8.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/movieDB/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/movieDB/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/movieDB/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
